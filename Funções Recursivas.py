55011ef61540c967a9b4e0cffd11467e32102d94
fernansoft <54730133+fernansoft@users.noreply.github.com>
2019-10-13 22:26:52 -0300

    Update Funções Recursivas.py

--- Funções Recursivas.py
+++ Funções Recursivas.py
@@ -2,42 +2,53 @@
 num = int(input('insira um número positivo desejado:'))
 def ffat(n):
   if (n==1):
-    return n  
+    return n
   else:
     f = (n * ffat(n-1))
   return f
 print ('valor fatorial', ffat(num))
 
-#Exercicio_01
+#serie01
 num = int(input('insira um número positivo:'))
 while num < 0:
   print('insira apenas valores positivos!')
   num = int(input('insira um número positivo'))
-def fsum(n):
+def fs01(n):
   if (n >= 100):
     return n
   else:
-    s = (n + fsum(n + 1))
+    s = (n + fs01(n + 1))
     return s
-print('A soma dos valores até 100 é:', fsum(num))
+print('A soma dos valores até 100 é:', fs01(num))
 
-#Exercicio_02
+#serie02
 num = int(input('insira o enésimo número: '))
-def fsumsub(n):
-  if n < 1:
+def fs02(n):
+  if (n < 1):
     return n
   else:
-    s = (n + fsumsub(n - 1))
+    s = (n + fs02(n - 1))
     return s
-print('O resultado é:', fsumsub(num))
+print('O resultado é:', fs02(num))
 
-#Exercicio_03
+#serie03
 num = float(input('digite o enésimo número: '))
-x = 1
-def fsumfrac(n):
-  if x >= n:
+def fs03(n):
+  if (n <= 1):
     return n
   else:
-    s = (1/x + fsumfrac(1/(x+1)))
+    s = ((n ** (-1)) + fs03((n-1) ** -1))
     return s
-print('o resultado é: ', fsumfrac(num))
+print('o resultado é: ', fs03(num))
+#naodeucerto aaaaa
+
+#serie04
+num = float(input('insira o enésimo número: '))
+denominador = 1
+def fs04(n):
+  if (n < 1):
+    return n
+  else:
+    s = ((n/denominador) + fs04((n - 1)/(denominador + 1)))
+    return s
+print('o resultado é: ', fs04(num))
